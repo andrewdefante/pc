ce3e74e0e53c9439dc95a41835e221a7f1dfc8bb
andrew.defante <andrew.defante1@gmail.com>
2026-01-05 20:55:05 -0800

    Initial SwiftUI app with SwiftData

---
+++ PitchChart/PitchChartApp.swift
@@ -0,0 +1,32 @@
+//
+//  PitchChartApp.swift
+//  PitchChart
+//
+//  Created by Andrew Defante on 1/5/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PitchChartApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PitchChartTests/PitchChartTests.swift
@@ -0,0 +1,16 @@
+//
+//  PitchChartTests.swift
+//  PitchChartTests
+//
+//  Created by Andrew Defante on 1/5/26.
+//
+
+import Testing
+
+struct PitchChartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
